b6c51db45c2f86695d16f564546ca1adcca5fb1b
Daniel <daniel@famlie-sturm.de>
2020-07-17 11:27:47 +0200

    finished mesh Generation for demo added demo_wo_mask.py (no masks)

--- meshrcnn/modeling/roi_heads/roi_heads.py
+++ meshrcnn/modeling/roi_heads/roi_heads.py
@@ -512,13 +512,16 @@ class MeshRCNNROIHeads(StandardROIHeads):
                 for feature in occ_features:
                     #i += 1
                     mesh, time = my_generator.generate_mesh(torch.unsqueeze(feature, 0))
+
                     # shortcut: save mesh
                     #mesh.export('/home/daniel/ADL4CV/meshes/mesh' + str(i) + '.obj')
                     meshes.append(mesh)
+                meshes_pyt3d = Meshes(verts=[torch.tensor(mesh.vertices) for mesh in meshes],
+                                   faces=[torch.tensor(mesh.faces) for mesh in meshes])
 
 
                 # TODO: append generated meshes to instances
-                occnet_mesh_rcnn_inference(meshes, instances)
+                mesh_rcnn_inference(meshes_pyt3d, instances)
 
 
 
